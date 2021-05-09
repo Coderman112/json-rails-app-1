ca22810786e52b489368a27f9024899559fd18f9
ChristianCudby <christiancudby@hotmail.com>
2021-05-09 16:08:03 -0400

    adds appendLists function to list.js

--- app-frontend/src/list.js
+++ app-frontend/src/list.js
@@ -2,7 +2,16 @@ function fetchLists(){
     fetch("http://localhost:3000/lists")
     .then(resp => resp.json())
     .then(lists => {
-        console.log(lists)
+        appendLists(lists)
     })
+}
+
+
+
+function appendLists(){
+    const listsDiv = document.getElementById('lists')
+    for (let list of lists) {
+        console.log(list)
+    }
 }
 
