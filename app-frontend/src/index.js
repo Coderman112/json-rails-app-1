beaeab611e6c61f330d1177a0f07a1de16197e83
ChristianCudby <christiancudby@hotmail.com>
2021-05-09 22:35:52 -0400

    adds return home function to index.js

--- app-frontend/src/index.js
+++ app-frontend/src/index.js
@@ -1,6 +1,22 @@
+let listForm = document.getElementById('listForm')
+
 listForm.addEventListener('submit', List.postList)
 List.fetchLists()
 
 function jsonToJS(resp){
     return resp.json()
+}
+
+function returnToHome(){
+    document.getElementById('listContainer').innerHTML = `
+        <form id="listForm">
+            <label>List Name:</label>
+            <input type="text">
+            <input type="submit" value="Submit List">
+        </form>
+        <div id="lists"></div>
+        `
+    listForm = document.getElementById('listForm')
+    listForm.addEventListener('submit', List.postList)
+    List.appendListsOnReturnHome()
 }
