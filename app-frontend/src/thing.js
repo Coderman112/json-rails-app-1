7769381b4b1bae3eb9c969eb1783b2e2cce14885
ChristianCudby <christiancudby@hotmail.com>
2021-05-09 20:14:58 -0400

    adds deleteThing function to thing.js

--- app-frontend/src/thing.js
+++ app-frontend/src/thing.js
@@ -5,11 +5,21 @@ function appendThings(things, element) {
     for (let thing of things) {
         const thingLi = document.createElement("li")
         const thingDelete = document.createElement("button")
-        todoDelete.innerText = "Delete"
+        thingDelete.innerText = "Delete"
+        thingDelete.id = thing.id
         thingLi.innerText = thing.content
-        thingDelete.addEventListener('click', deleteThing)
+        thingDelete.addEventListener('click', function(e) {
+            deleteThing(thing.id)
+        }) 
+        // thingDelete.addEventListener('click', deleteThing)
         thingLi.append(thingDelete)
         ul.append(thingLi)
     }
 
+}
+
+function deleteThing(thingId) {
+    fetch(`http://localhost:3000/things/${thingId}`, {
+        method: "DELETE"
+    })
 }
